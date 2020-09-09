466ec83f5db1c3b5cf2b4828fa4e4cd0c5e17226
Ash W <amsheppard2016@gmail.com>
2020-09-09 00:04:26 -0400

    day one done

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,24 @@
+   # item_choice = input('Choose [get] or [drop] followed by item name')
+# print(item_choice)
+
+# #try:
+#     #direction= str(direction_choice)
+
+#     #if direction_choice == 'n':
+#         #player.current_room = player.current_room.n_to
+#         #print(room.name, room.description, room.item, item_choice)
+#         #if item_choice == f"get {item.name}":
+#             #on_take(item.name)
+#             #add item to player inventory
+#         #if item_choice == f"drop {item.name}":
+#             #on_drop(item.name)
+#             #leave item in room invenory
+#         # if item_choice != f"get {item.name}" or f"drop {item.name}":
+#         #     print("Unknown Command",item_choice)
+
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -33,6 +53,30 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+
+# Declare all the items
+item = {
+    'anthrax':  Item("Anthrax",
+                     """A serious infectious disease. This will kill in 2 turns, but do be careful it is highly contagious"""),
+
+    'bazooka':    Item("Bazooka", """Anti-tank rocket launcher to blow your enemies away."""),
+
+    'gun': Item("Gun", """A short barrelled fire arm, that is great for wounding enemies"""),
+
+    'sword':   Item("Sword", """A long metal blade, that can cause major injuries."""),
+
+    'diamond': Item("Diamond", """A precious stone worth more than you make in a year, right at your finger tips"""),
+}
+
+# Link item to room
+
+room['outside'].items_loc = [item['anthrax']]
+room['foyer'].items_loc = [item['sword']]
+room['overlook'].items_loc = [item['bazooka']]
+room['narrow'].items_loc = [item['gun']]
+room['treasure'].items_loc = [item['diamond']]
+
+
 #
 # Main
 #
@@ -49,3 +93,61 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# item_choice = input('Choose [get] or [drop] followed by item name')
+# print(item_choice)
+
+def new_player():
+    Player.name = "You"
+    Player.current_room = room['outside']
+    print(f'{Player.name} you are located {Player.current_room}')
+
+new_player()
+
+
+def player_options():
+    direction_choice = input(f'Choose [n] North, [s] South, [e] East, [w] West, [q] Quit:')
+
+    try:
+        if direction_choice == 'n':
+            if Player.current_room.n_to == '':
+                print(f'Ran into wall please choose a different direction')
+            else:
+                Player.current_room = Player.current_room.n_to
+                print(Player.current_room)
+            player_options()
+                    
+        elif direction_choice == 's':
+            if Player.current_room.s_to == '':
+                print(f'Ran into wall please choose a different direction')
+            else:
+                Player.current_room = Player.current_room.s_to
+                print(Player.current_room)
+            player_options()
+
+        elif direction_choice == 'e':
+            if Player.current_room.e_to == '':
+                print(f'Ran into wall please choose a different direction')
+            else:
+                Player.current_room = Player.current_room.e_to
+                print(Player.current_room)
+            player_options()
+
+        elif direction_choice == 'w':
+            if Player.current_room.w_to == '':
+                print(f'Ran into wall please choose a different direction')
+            else:
+                Player.current_room = Player.current_room.w_to
+                print(Player.current_room)
+            player_options()
+
+        elif direction_choice == 'q':
+            print('Thanks for playing.')
+            #add break point    
+    except ValueError:
+        print(f'Please enter a valid choice')
+
+player_options()
+
+
+
--- src/player.py
+++ src/player.py
@@ -1,8 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
 class Player:
-    def __init__(self, name, room):
-        self.name= name
-        self.room= room 
+    def __init__(self, name="player1", current_room="outside", items=[]):
+        self.name = name
+        self.current_room = current_room
+        self.items = items
+
     def __str__(self):
-        return f'{self.name} is in the {self.room}'
+        return f'{self.name} is in the {self.current_room} with {self.items}'
+
+#     def print_location:
+#         print(f'You are in {self.current_room}')
+    
+#     def print_inventory:
+#         print(f'You have {self.items} in your inventory')
+
+#     def add_item:
+        # pass
+#     def drop_item:
+        # pass
